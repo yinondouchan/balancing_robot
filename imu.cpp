f0c08ba141d3f4edcaf8a9f2c9faebc294a01b3e
Yinon Douchan <yinondou@gmail.com>
2019-09-09 21:48:20 +0300

    organized imu module

--- imu.cpp
+++ imu.cpp
@@ -23,13 +23,10 @@ float cf_angle_x;
 float cf_angle_y;
 float cf_angle_z;
 
+// angle obtained by integrating the gyro values
 float gyro_angle_x;
-float accel_lpf_x;
 
-int32_t prev_cf_angle_x;
-int32_t prev_cf_angle_y;
-int32_t prev_cf_angle_z;
-
+// init the IMU
 void imu_init(MPU_9250 *imu)
 {
     // init imu
@@ -38,12 +35,14 @@ void imu_init(MPU_9250 *imu)
 
     // enable only accel and gyro
     imu->setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL);
-    
+
+    // set gyro and accelerometer full scale values
     gyro_full_scale = 2000;
     accel_full_scale = 8;
     imu->setGyroFullScaleRange(gyro_full_scale);
     imu->setAccelFullScaleRange(accel_full_scale);
 
+    // init gyro biases
     gyro_bias_x = 0;
     gyro_bias_y = 0;
     gyro_bias_z = 0;
@@ -56,20 +55,21 @@ void read_accel_and_gyro(MPU_9250 *imu)
     imu->getAccelReg(imu_raw_data.accel[0], timestamp);
     imu->getGyroReg(imu_raw_data.gyro[0], timestamp);
 
-    // convert data from raw to g for accel and dps for gyro
+    // convert gyro data from raw to dps
     imu_data.gyro[0] = (float)imu_raw_data.gyro[0] * gyro_full_scale / 32768 - gyro_bias_x;
     imu_data.gyro[1] = (float)imu_raw_data.gyro[1] * gyro_full_scale / 32768 - gyro_bias_y;
     imu_data.gyro[2] = (float)imu_raw_data.gyro[2] * gyro_full_scale / 32768 - gyro_bias_z;
 
+    // convert accelerometer data from raw to g
     imu_data.accel[0] = (float)imu_raw_data.accel[0] * accel_full_scale / 32768;
     imu_data.accel[1] = (float)imu_raw_data.accel[1] * accel_full_scale / 32768;
     imu_data.accel[2] = (float)imu_raw_data.accel[2] * accel_full_scale / 32768;
 }
 
+// find the measurement biases of the gyroscope by averaging over samples
 void imu_calibrate_gyro(MPU_9250 *imu)
 {
     int i=0;
-    
     float bias_sum_x = 0;
     float bias_sum_y = 0;
     float bias_sum_z = 0;
@@ -90,32 +90,31 @@ void imu_calibrate_gyro(MPU_9250 *imu)
     gyro_bias_z = bias_sum_z / GYRO_CALIBRATION_NSAMPLES;
 }
 
+// init complementary filter
 void compl_filter_init()
 {   
+    // initialize angle to the one read from the accelerometer - this is a fast, though noisy way to initialize the angle
     cf_angle_x = get_angle_from_accelerometer();
     gyro_angle_x = cf_angle_x;
-    cf_angle_y = 0;
-    cf_angle_z = 0;
 }
 
+// get angle from the accelerometer only (fast response, but noisy)
 float get_angle_from_accelerometer()
 {
+    // an arc-tan a day reads the angle away
     return atan2(imu_data.accel[1], imu_data.accel[2]) * 180 / M_PI;
 }
 
+// read gyro and accelerometer, apply complementary filter
+// return angle and angular velocities in deg and deg/sec respectively
 void compl_filter_read(int32_t dt_micros)
-{   
+{  
+    // only read x value since there is no need in other values (yet)
+   
     // calculate accel angle
     float angle_float_x = get_angle_from_accelerometer();
-    //float angle_float_y = atan2(-imu->a_raw.z, imu->a_raw.x) * 180 / M_PI;
-    //float angle_float_z = atan2(-imu->a_raw.y, imu->a_raw.x) * 180 / M_PI;
     
     // apply complementary filter 300 us
     gyro_angle_x += imu_data.gyro[0]*dt_micros / 1000000;
     cf_angle_x = CF_CONST*(cf_angle_x + imu_data.gyro[0]*dt_micros/1000000) + (1.0 - CF_CONST)*angle_float_x;
-    //cf_angle_y = CF_CONST*(cf_angle_y + unbiased_gyro_y*dt_micros/1000000) + (1.0 - CF_CONST)*accel_angle_y;
-    //cf_angle_z = CF_CONST*(cf_angle_z + unbiased_gyro_z*dt_micros/1000000) + (1.0 - CF_CONST)*accel_angle_z;
-    //prev_cf_angle_x = cf_angle_x;
-    //prev_cf_angle_y = cf_angle_y;
-    //prev_cf_angle_z = cf_angle_z;
 }
